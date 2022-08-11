b5e5c45e907db403c56f7dcac8d94dbe95cb3ff6
iamoki <pureaoh@gmail.com>
2022-08-11 12:25:40 +0900

    ✅ solution15 - 다른방법 추가

--- solution15.js
+++ solution15.js
@@ -1,12 +1,29 @@
 // 가운데 문자 출력하기
+// function solution(s) {
+//   let answer;
+//   let middle = Math.floor(s.length/2);
+  
+//   if(s.length%2 === 0) {
+//     answer = s[middle - 1] + s[middle]
+//   } else {
+//     answer = s[middle];
+//   }
+
+//   return answer; 
+// }
+
+// console.log(solution("study"));
+
+
+// 메소드를 사용한 방법
 function solution(s) {
   let answer;
   let middle = Math.floor(s.length/2);
   
   if(s.length%2 === 0) {
-    answer = s[middle - 1] + s[middle]
+    answer = s.substring(middle - 1, middle + 1);
   } else {
-    answer = s[middle];
+    answer = s.substring(middle, middle + 1);
   }
 
   return answer; 
