1fa7319c992dccb74b1f448fd80d17999d9cb2a0
iamoki <pureaoh@gmail.com>
2022-08-11 13:39:22 +0900

    ✅ solution17

---
+++ solution17.js
@@ -0,0 +1,31 @@
+// 중복문자 제거하기
+// function solution(s) {
+//   let answer = [];
+//   for(let i = 0; i < s.length; i++) {
+//     // console.log(s[i], i, s.indexOf(s[i]));
+//     if(i === s.indexOf(s[i])) {
+//       answer.push(s[i]);
+//     }
+//   }
+//   return answer;
+// }
+
+// let arr = ['good', 'time', 'good', 'time', 'student']
+// console.log(solution(arr));
+
+
+// 고차함수 사용하기
+function solution(s) {
+  let answer = [];
+  s.filter((i, idx, arr) => {
+    // console.log(i, idx, arr.indexOf(i))
+    if(idx === arr.indexOf(i)) {
+      answer.push(i);
+    }
+  })
+
+  return answer;
+}
+
+let arr = ['good', 'time', 'good', 'time', 'student']
+console.log(solution(arr));
