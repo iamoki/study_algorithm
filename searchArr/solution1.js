c411e5d1b0eb8b3cbd4a928c88681c7bc6cf05cb
iamoki <pureaoh@gmail.com>
2022-08-11 14:29:39 +0900

    ✅ solution1

--- searchArr/solution1.js
+++ searchArr/solution1.js
@@ -1,7 +1,15 @@
-// 중복문자 제거하기
-function solution(s) {
-  let answer;
+// 앞에보다 큰수 출력하기
+function solution(arr) {
+  let answer = [];
+  answer.push(arr[0]);
+
+  for(let i = 1; i < arr.length; i++) {
+    // console.log(arr[i], arr[i-1])
+    if(arr[i] > arr[i-1]) answer.push(arr[i]);
+  }
+
   return answer;
 }
 
-console.log(solution());
+let arr = [7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
