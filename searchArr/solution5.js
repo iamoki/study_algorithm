f7f88a365ed85cf5c75e75dfeb026ac55757667e
iamoki <pureaoh@gmail.com>
2022-08-13 21:38:42 +0900

    ✅ solution5

---
+++ searchArr/solution5.js
@@ -0,0 +1,21 @@
+// 가위바위보
+function solution(arr) {
+  let answer = Array.from({length: arr.length}, () => 1);
+
+  for(let i = 0; i < arr.length; i++) {
+    for(let x = 0; x < arr.length; x++) {
+      console.log('arr i:' + arr[i]);
+      console.log('arr x:' + arr[x]);
+      console.log('----------------')
+      if(arr[i] < arr[x]) {
+        answer[i]++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+
+let arr = [87, 89, 92, 100, 76];
+console.log(solution(arr));
