04f1c4cc10785e6972930d36268cd97be9a1507e
iamoki <pureaoh@gmail.com>
2022-08-13 16:26:04 +0900

    ✅ solution4

---
+++ searchArr/solution4.js
@@ -0,0 +1,21 @@
+// 가위바위보
+function solution(a, b) {
+  let answer = 0;
+  let cnt = 0;
+
+  for(let i = 0; i < a.length; i++) {
+    if(a[i] === 1) {
+      cnt++;
+      answer += cnt;
+    } else {
+      cnt = 0;
+    }
+  }
+  
+  
+  return answer;
+}
+
+let a = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+
+console.log(solution(a));
